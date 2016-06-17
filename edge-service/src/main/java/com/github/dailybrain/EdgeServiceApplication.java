6771871936da30a223a9d6a302ab4d594a6cbc61
Vincent Spiewak <vspiewak@gmail.com>
2016-06-17 15:25:43 +0200

    Pass query parameters to edge-service /names

--- edge-service/src/main/java/com/github/dailybrain/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/github/dailybrain/EdgeServiceApplication.java
@@ -1,6 +1,8 @@
 package com.github.dailybrain;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,16 +16,20 @@ import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 
@@ -50,15 +56,13 @@ public class EdgeServiceApplication {
 @RequestMapping("/reservations")
 class ReservationEdgeController {
 
+    private static final Logger log = LoggerFactory.getLogger(ReservationEdgeController.class);
+
     private final RestTemplate restTemplate;
 
     @Autowired
     public ReservationEdgeController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-    }
-
-    public Collection<String> fallback() {
-        return new ArrayList<>();
     }
 
     @Autowired
@@ -73,10 +77,24 @@ class ReservationEdgeController {
         );
     }
 
+    public Collection<String> fallback(MultiValueMap<String,String> params) {
+        return new ArrayList<>();
+    }
+
     @HystrixCommand(fallbackMethod = "fallback")
     @RequestMapping(method = RequestMethod.GET, value = "/names")
-    public Collection<String> names() {
-        return this.restTemplate.exchange("http://reservation-service/reservations",
+    public Collection<String> names(@RequestParam MultiValueMap<String,String> params) {
+
+        log.info("GET /names params: {}", params);
+
+        URI uri = UriComponentsBuilder
+                .fromHttpUrl("http://reservation-service/reservations")
+                .queryParams(params)
+                .build()
+                .encode()
+                .toUri();
+
+        return this.restTemplate.exchange(uri,
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<Resources<Reservation>>() {
